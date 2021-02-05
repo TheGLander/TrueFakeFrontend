0d2508b374078a486979b635dc994605bf177e70
Lubomir <lubomir.yudenko@gmail.com>
2021-02-05 23:30:36 +0200

    Add new typography description

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,47 @@
+import Typography from "@material-ui/core/Typography"
+import Grid from "@material-ui/core/Grid"
 import React from "react"
-import FetchWrapper from "./FetchWrapper"
 import NewsTestForm from "./NewsTestForm"
-import PredictFetch from "./PredictFetch"
+import {
+	createStyles,
+	Theme,
+	withStyles,
+	WithStyles,
+} from "@material-ui/core/styles"
 
-export default class App extends React.Component {
+const styles = (theme: Theme) =>
+	createStyles({
+		typography: {
+			"&": {
+				margin: theme.spacing(1),
+				marginTop: theme.spacing(2),
+				width: "70ch",
+			},
+		},
+		img: { "&": { margin: theme.spacing(2) } },
+	})
+
+interface Props extends WithStyles<typeof styles> {}
+
+class App extends React.Component<Props, {}> {
 	render(): JSX.Element {
-		return <NewsTestForm />
+		const styles = this.props.classes
+		return (
+			<Grid container justify="center" direction="column" alignItems="center">
+				<img
+					src="https://glander.club/true-fake-picture.png"
+					className={styles.img}
+				></img>
+				<NewsTestForm />
+				<Typography className={styles.typography}>
+					Це - детектор новин про коронавірус. Для того, щоби дізнатися, чи є
+					новина фейком або правдою, введіть її заголовок (необов'язкове поле)
+					та текст (обов'язкове поле), після чого натисніть "Дізнатися правду!".
+					Детектор повідомить вам, наскільки він впевнений у своєму прогнозі.
+				</Typography>
+			</Grid>
+		)
 	}
 }
+
+export default withStyles(styles)(App)
